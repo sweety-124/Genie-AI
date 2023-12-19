e1aca41a4fb1564ff9e1e0f74e4e10c8cc6bcbf7
Sweety Santhosh <ss9334464@gmail.com>
2023-12-19 23:30:59 +0400

    chnages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "90249838ecd91cb1"; }
+/******/ 		__webpack_require__.h = function() { return "40ded88629ee83a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
